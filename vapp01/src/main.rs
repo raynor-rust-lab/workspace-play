dfda44bb3c10200374cb8977fa647b4a559a4c46
raynor <raynor.yim@gmail.com>
2024-11-25 07:45:29 +0800

    chore: init project

---
+++ vapp01/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    print();
+}
+
+fn print() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_print() {
+        // Capture the output of the print function
+        let output = std::panic::catch_unwind(|| {
+            print();
+        });
+
+        // Check if the function executed without panicking
+        assert!(output.is_ok());
+    }
+}
